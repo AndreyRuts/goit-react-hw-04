016a1c12c1738b816323ac5566f58284d02b37fe
Xandr <ravnb1989@gmail.com>
2025-01-17 15:53:36 +0200

    gallery 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,45 @@ import ImageGallery from './components/ImageGallery/ImageGallery';
 import Loader from './components/Loader/Loader';
 import ErrorMessage from './components/ErrorMessage/ErrorMessage';
 import ImageModal from './components/ImageModal/ImageModal';
+import { useState, useEffect } from 'react';
+import { fetchData } from './services/api';
+
 
 const App = () => {
+    const [gallery, setGallery] = useState([]);
+    const [searchData, setSearchData] = useState('');
+    const [page, setPage] = useState(1);
+
+
+    useEffect(() => {
+        if (searchData === '') return;
+
+        const getData = async () => {
+            try {
+                const { data } = await fetchData(searchData, page);
+                if (data.total === 0) return;
+                setGallery((prevGallery) => {
+                    return [...prevGallery, ...data.results];
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getData();
+    }, [page ,searchData]);
+
+
+    const handleQuery = (newQuery) => {
+        setSearchData(newQuery);
+        setGallery([]);
+        setPage(1);
+    }
+
 
     return (
         <> 
-            <SearchBar />
-            <ImageGallery />
+            <SearchBar onSubmit={handleQuery}/>
+            <ImageGallery gallery={gallery} />
             <Loader />
             <ErrorMessage />
             <LoadMoreBtn />
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,40 @@
-// import s from './SearchBar.module.css';
+import s from './SearchBar.module.css';
 
-const SearchBar = () => {
+const SearchBar = ({ onSubmit }) => {
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        const customQuery = evt.target.customQuery.value.trim();
+
+        if (!customQuery) {
+            console.log("OMG ERROR");
+            return;
+        }
+        onSubmit(customQuery);
+        console.log(customQuery);
+        
+    };
+
+
+
+
+
     return (
         <header>
-            <form>
-                <input
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                />
-                <button type="submit">Search</button>
+            <form className={s.form} onSubmit={handleSubmit}>
+                <div className={s.wrapper}>
+                    <input
+                        className={s.input}
+                        type="text"
+                        name="customQuery"
+                        autoComplete="off"
+                        autoFocus
+                        placeholder="Search images and photos"
+                    />
+                    <button type="submit">Search</button>
+                </div>
             </form>
         </header>
     );
-}
+};
 
 export default SearchBar;
